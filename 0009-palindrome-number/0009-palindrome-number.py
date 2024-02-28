d9a76720ac9e529334b097aeb7192ec7b7aa3dd4
Vika <palvanshika609@gmail.com>
2024-02-28 20:48:34 +0530

    Time: 51 ms (28.14%), Space: 11.6 MB (83.76%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        return True if str(x) == str(x)[::-1] else False
+        """
+        :type x: int
+        :rtype: bool
+        """
+        
