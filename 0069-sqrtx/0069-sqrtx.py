6673341401be68e882722874d43ba78600681921
Vika <palvanshika609@gmail.com>
2024-03-19 20:59:58 +0530

    Time: 16 ms (70.30%), Space: 11.6 MB (38.51%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def mySqrt(self, x):
+        return int(floor(math.sqrt(x))) if x>=0 else false
+        """
+        :type x: int
+        :rtype: int
+        """
+        
